10e794163645b7bc62d29bf4d2bbf5165662b1df
MUHAMMED FAYIS K <132967218+Fayisfa@users.noreply.github.com>
2025-08-25 22:17:22 +0000

    api changed to hosted version

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,9 @@
 
 import type { Submission } from "./types";
 
-const API_BASE_URL = 'http://localhost:8080';
+// const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://mediumaquamarine-eel-107102.hostingersite.com';
+
 
 /**
  * Handles the login for a State Admin.
